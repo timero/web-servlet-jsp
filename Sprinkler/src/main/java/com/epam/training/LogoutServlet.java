5facf522674d1429f3503aa8435d8c1222637b62
Szabolcs Foti <szabolcs_foti@epam.com>
2016-04-20 14:17:29 +0200

    fix logout bug

--- Sprinkler/src/main/java/com/epam/training/LogoutServlet.java
+++ Sprinkler/src/main/java/com/epam/training/LogoutServlet.java
@@ -7,15 +7,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @WebServlet("/admin/logout")
 public class LogoutServlet extends HttpServlet {
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		req.logout();
-		resp.sendRedirect("Sprinkler/admin/index");
-	}
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.logout();
+        resp.sendRedirect("/Sprinkler/admin/index");
+    }
 }
