917202298120f3fee2b09df494e065ed61ecd24a
Bence Garamvolgyi <Bence_Garamvolgyi@epam.com>
2016-04-20 14:02:54 +0200

    fix login bug

--- Sprinkler/src/main/java/com/epam/training/LoginPageServlet.java
+++ Sprinkler/src/main/java/com/epam/training/LoginPageServlet.java
@@ -1,13 +1,14 @@
 package com.epam.training;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet({"/login", "/loginFail", ""})
+@WebServlet({"/login", "/loginFail"})
 public class LoginPageServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -15,6 +16,7 @@ public class LoginPageServlet extends HttpServlet {
         super();
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("views/login.jsp").forward(request, response);
     }
